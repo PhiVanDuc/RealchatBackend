8ac184f5300659884b78082f92a6f31a1c978787
Phi Duc <phivanduc325@gmail.com>
2025-08-03 17:18:36 +0700

    Fix cors

--- libs/cors.js
+++ libs/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://realchat-three.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     optionsSuccessStatus: 200,
     credentials: true
